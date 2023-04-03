90bb7deeb91ee321f0469c9268482396610b3be3
Maksim Zabelin <75933870+mzabelin8@users.noreply.github.com>
2023-04-03 14:02:14 +0300

    Update sum_func.c

--- sum_func.c
+++ sum_func.c
@@ -2,20 +2,7 @@
 #include <stdlib.h>
 #include <omp.h>
 
-void matrix_sum() {
-    int m, n;
-    printf("Введите количество строк и столбцов матриц: ");
-    scanf("%d %d", &m, &n);
-
-    int **matrix1 = (int **)malloc(m * sizeof(int *));
-    int **matrix2 = (int **)malloc(m * sizeof(int *));
-    int **result = (int **)malloc(m * sizeof(int *));
-    for (int i = 0; i < m; i++) {
-        matrix1[i] = (int *)malloc(n * sizeof(int));
-        matrix2[i] = (int *)malloc(n * sizeof(int));
-        result[i] = (int *)malloc(n * sizeof(int));
-    }
-
+void matrix_sum(int m, int n, int **matrix1, int **matrix2, int **result) {
     printf("Заполнение матриц случайными значениями...\n");
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
@@ -30,22 +17,4 @@ void matrix_sum() {
             result[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
-
-    printf("\nРезультат сложения матриц:\n");
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", result[i][j]);
-        }
-        printf("\n");
-    }
-
-    for (int i = 0; i < m; i++) {
-        free(matrix1[i]);
-        free(matrix2[i]);
-        free(result[i]);
-    }
-    free(matrix1);
-    free(matrix2);
-    free(result);
 }
-
