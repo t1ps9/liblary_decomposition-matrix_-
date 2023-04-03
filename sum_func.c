fa4409dea103bf3df0873d57e472d571b6763971
Maksim Zabelin <75933870+mzabelin8@users.noreply.github.com>
2023-04-03 14:04:43 +0300

    Update sum_func.c

--- sum_func.c
+++ sum_func.c
@@ -3,13 +3,15 @@
 #include <omp.h>
 
 void matrix_sum(int m, int n, int **matrix1, int **matrix2, int **result) {
-    printf("Заполнение матриц случайными значениями...\n");
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            matrix1[i][j] = rand() % 10;
-            matrix2[i][j] = rand() % 10;
-        }
-    }
+//     printf("Заполнение матриц случайными значениями...\n");
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             matrix1[i][j] = rand() % 10;
+//             matrix2[i][j] = rand() % 10;
+//         }
+//     }
+    
+    //    можно оставить рандомное заполнение матриц , а можно и передовать в функцию из main
 
     #pragma omp parallel for
     for (int i = 0; i < m; i++) {
