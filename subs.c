67182a0ea8ec6eaf91ab68cbe346b56a266cb502
Maksim Zabelin <75933870+mzabelin8@users.noreply.github.com>
2023-06-05 15:16:02 +0300

    Create subs.c

---
+++ subs.c
@@ -0,0 +1,17 @@
+// Function to subtract two matrices
+Matrix* subtractMatrix(Matrix* m1, Matrix* m2) {
+    if(m1->rows != m2->rows || m1->cols != m2->cols) {
+        printf("Error: Matrices must have the same dimensions for subtraction.\n");
+        return NULL;
+    }
+
+    Matrix* result = createMatrix(m1->rows, m1->cols);
+
+    for(int i = 0; i < m1->rows; i++) {
+        for(int j = 0; j < m1->cols; j++) {
+            result->data[i][j] = m1->data[i][j] - m2->data[i][j];
+        }
+    }
+
+    return result;
+}
