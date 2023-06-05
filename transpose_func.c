c626f7fb646cd9730b0f2f92e5b4aab914d3482a
Maksim Zabelin <75933870+mzabelin8@users.noreply.github.com>
2023-06-05 15:02:43 +0300

    Update transpose_func.c

--- transpose_func.c
+++ transpose_func.c
@@ -1,10 +1,12 @@
-void transpose(int matrix[][MAX], int rows, int cols) {
-    int i, j, temp;
-    for (i = 0; i < rows; i++) {
-        for (j = i + 1; j < cols; j++) {
-            temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
+// Function to transpose a matrix
+Matrix* transposeMatrix(Matrix* m) {
+    Matrix* result = createMatrix(m->cols, m->rows);
+
+    for(int i = 0; i < m->rows; i++) {
+        for(int j = 0; j < m->cols; j++) {
+            result->data[j][i] = m->data[i][j];
         }
     }
+
+    return result;
 }
