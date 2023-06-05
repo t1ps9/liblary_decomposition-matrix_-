c118450993af58d37aa6edd308bda5e7b2947d8d
Maksim Zabelin <75933870+mzabelin8@users.noreply.github.com>
2023-06-05 15:01:10 +0300

    Update det.c

--- det.c
+++ det.c
@@ -1,30 +1,34 @@
-int determinant(int matrix[][N], int n) {
-    int det = 0;
-    int submatrix[N][N];
-    
-    if (n == 1) {
-        return matrix[0][0];
-    } else if (n == 2) {
-        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
-    } else {
-        for (int k = 0; k < n; k++) {
-            int i = 0, j = 0;
-            
-            for (int row = 1; row < n; row++) {
-                for (int col = 0; col < n; col++) {
-                    if (col == k) {
-                        continue;
-                    }
-                    submatrix[i][j++] = matrix[row][col];
-                    if (j == n - 1) {
-                        i++;
-                        j = 0;
-                    }
-                }
-            }
-            det += (k % 2 == 0 ? 1 : -1) * matrix[0][k] * determinant(submatrix, n - 1);
-        }
+// Function to calculate the determinant of a matrix
+int determinant(Matrix* m) {
+    if (m->rows != m->cols) {
+        printf("Error: The determinant is only defined for square matrices.\n");
+        return 0;
     }
-    
+
+    if (m->rows == 1) {
+        return m->data[0][0];
+    } else if (m->rows == 2) {
+        return m->data[0][0]*m->data[1][1] - m->data[0][1]*m->data[1][0];
+    }
+
+    int det = 0;
+    for(int i = 0; i < m->rows; i++) {
+        Matrix* submatrix = createMatrix(m->rows-1, m->cols-1);
+
+        for(int j = 1; j < m->rows; j++) {
+            int offset = 0;
+            for(int k = 0; k < m->cols; k++) {
+                if(k == i) {
+                    offset = 1;
+                    continue;
+                }
+                submatrix->data[j-1][k-offset] = m->data[j][k];
+            }
+        }
+
+        det += (i % 2 ? -1 : 1) * m->data[0][i] * determinant(submatrix);
+        freeMatrix(submatrix);
+    }
+
     return det;
 }
